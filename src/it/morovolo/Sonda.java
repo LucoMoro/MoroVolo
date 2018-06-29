537970e61ecab295c28b9cabc367d681244de983
Luca <Luca@192.168.5.143>
2018-06-29 09:38:10 +0200

    aggiustamenti

--- src/it/morovolo/Sonda.java
+++ src/it/morovolo/Sonda.java
@@ -10,19 +10,19 @@ public class Sonda {
 	public static void main(String[] args) throws InterruptedException {
 		if(!(args.length < 3)) {//verifica mancanza del primo argomento: pa livello mare
 			double  percentuale=Double.parseDouble(args[2]);//altezza paracadute nuova
-			double x = Double.parseDouble(args[1]);//altezza sgancio nuova
+			double altezzaDiSganciov2 = Double.parseDouble(args[1]);//altezza sgancio nuova
 			int altezzaDiSgancio;
 			Barometro b = new Barometro(Double.parseDouble(args[0]));//definizione di pressioneAttuale e pressioneLivelloMare. parte il thread
 			b.getAltezza();//ci stiamo passando il parametro della classe Barometro
 			System.out.println("l'altezza è di " + b.getAltezza() + " m ");//facciamo comparire a video a quanti metri siamo
 			/**qui andremo a capire quando la sondra dovrà sganciarsi*/
-			System.out.println("la sonda si sgancerà a " + x);
-			altezzaDiSgancio= (int) ((percentuale*x)/100);
+			System.out.println("la sonda si sgancerà a " + altezzaDiSganciov2);
+			altezzaDiSgancio= (int) ((percentuale*altezzaDiSganciov2)/100);
 			System.out.println("il paracadute si aprirà a " + altezzaDiSgancio);
-			while (b.getAltezza() < x) {
+			while (b.getAltezza() < altezzaDiSganciov2) {
 				Thread.sleep(10);//sleep serve per il riposo
 			}
-			System.out.println("\nLa sonda si è sganciata all altitudine di " + x + " m\n");//qui comparirà a video a quanti metri la sonda si sarà sganciata
+			System.out.println("\nLa sonda si è sganciata all altitudine di " + altezzaDiSganciov2 + " m\n");//qui comparirà a video a quanti metri la sonda si sarà sganciata
 			/**qui andremo a capire quando si aprià il paracadute*/
 			while(b.getAltezza() > altezzaDiSgancio) {
 				Thread.sleep(10);//sleep serve per il riposo
